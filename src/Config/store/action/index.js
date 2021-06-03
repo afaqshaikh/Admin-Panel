44c0ac89cfaedbdd3189eb53a53a82fc67102aa5
afaqshaikh <zaidsmai920@hotmail.com>
2021-06-03 11:31:44 +0500

    Create add account function

--- src/Config/store/action/index.js
+++ src/Config/store/action/index.js
@@ -1,13 +1,4 @@
 
-// const set_account = (name,contact) => {
-//     console.log(name,contact)
-//     var newAccount = {name : name,contact:contact}
-//     console.log(newAccount)
-//     return (dispatch) => {
-//         console.log("Working......")
-//         dispatch({type : 'SETACCOUNT',payload : newAccount})
-//     }
-// }
 const set_account = (newAccount) => {
     console.log("Setaccount==>",newAccount)
     return (dispatch) => {
